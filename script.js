7c6cac4174c27a23c555c17868890ff50ebc8df3
emi-220 <omer.wya220@gmail.com>
2019-10-05 15:33:20 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+$(function(){
+  $(".main_visual img:not(:first-child)").hide();
+  setInterval(function(){
+    $(".main_visual img:first-child").fadeOut("slow")
+    .next("img").fadeIn("slow").end().appendTo(".main_visual");
+  },5000);
+});
+
+
+$(document).ready(function(){
+  $(".slide").slick({
+  dots: false,
+  arrows: true,
+  centerMode: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+});
+  
+  $(".slide").slick({
+    responsive:({
+      breakpoint: 768,
+      settings:{
+        dots: false,
+        arrows: true,
+        centerMode: true,
+        slideTsshow: 3,
+        slideToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+      }
+    })
+  });
+});
+
