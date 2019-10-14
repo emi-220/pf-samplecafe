dddf90f9b0593968fecbcf22bb1749df85186e10
emi-220 <omer.wya220@gmail.com>
2019-10-14 14:17:50 +0900

    js修正

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// (function(){
-//   $(".main_visual img:not(:first-child)").hide();
-//   setInterval(function(){
-//     $(".main_visual img:first-child").fadeOut("slow")
-//     .next("img").fadeIn("slow").end().appendTo(".main_visual");
-//   },5000);
-// });
-
-  $(document).ready(function(){
-    $(".main_visual").slick({
-    dots: false,
-    arrows: false,
-    fade: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-  });
+$(function(){
+  $(".main_visual img:not(:first-child)").hide();
+  setInterval(function(){
+    $(".main_visual img:first-child").fadeOut("slow")
+    .next("img").fadeIn("slow").end().appendTo(".main_visual");
+  },5000);
 });
+
+//   $(document).ready(function(){
+//     $(".main_visual").slick({
+//     dots: false,
+//     arrows: false,
+//     fade: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 5000,
+//   });
+// });
 
 
 
